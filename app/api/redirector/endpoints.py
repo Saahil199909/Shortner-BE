df9e74ad2a01a316ebe9042e24733227381c6912
sahil <sahilchettiar111@gmail.com>
2024-11-26 23:10:32 +0530

    added location

--- app/api/redirector/endpoints.py
+++ app/api/redirector/endpoints.py
@@ -12,7 +12,8 @@ from app.api.redirector.utils import collect_short_link_details
 
 
 project_rootpath = os.path.dirname(os.path.abspath(__file__))
-maxmind_db_path = os.path.join(project_rootpath, 'GeoLite2-City_20240809/GeoLite2-City.mmdb' )
+maxmind_db_path = os.path.join(project_rootpath, 'GeoLite2-City_20240809', 'GeoLite2-City.mmdb' )
+print(maxmind_db_path, "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmm")
 
 router = APIRouter()
 
@@ -20,8 +21,10 @@ router = APIRouter()
 @router.get('/{short_key}')
 async def redirect_short_url(short_key: str, request: Request, db: Session = Depends(get_db)):
     try: 
+        print(short_key, "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
         key_length = len(short_key)
         model_name = SHORT_LINK_DETAILS_MODELS.get(key_length)
+        print(model_name, "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
         result = db.query(model_name).filter(model_name.short_key == short_key, model_name.is_deleted == False).first()
         long_url = result.long_url
 
