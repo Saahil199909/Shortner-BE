2ec3ed42fdb5e85728c7761c45a04782aded5f7e
sahil <sahilchettiar111@gmail.com>
2024-11-26 13:10:09 +0530

    getting values from env

--- app/config.py
+++ app/config.py
@@ -1,11 +1,17 @@
-DB_HOST = 'localhost'
-DB_USERNAME = 'Hexa-l3'
-DB_PASSWORD = 'Hexa'
-DB_DATABASE = 'shortner'
+from dotenv import load_dotenv
+import os
 
-REDIS_HOST = 'localhost'
-REDIS_PORT = '6379'
-REDIS_DB = 10
+load_dotenv()
+
+
+DB_HOST = os.getenv('DB_HOST')
+DB_USERNAME =  os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_DATABASE = os.getenv('DB_DATABASE')
+
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = '6379'
+# REDIS_DB = 10
 
 POSTGRESS_DATABASE_URL = f'postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}'
-
+print("Postgres URL:", POSTGRESS_DATABASE_URL)
